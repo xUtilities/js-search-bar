553a8c2272a694cfbb5c4dde9f58e0b93925ad2c
xUtilities <manmrisaguy@gmail.com>
2025-01-09 23:25:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,7 @@ let users = []
 searchInput.addEventListener("input", e => {
   const value = e.target.value.toLowerCase()
   users.forEach(user => {
-    const isVisible =
-      user.name.toLowerCase().includes(value) ||
-      user.email.toLowerCase().includes(value)
+    const isVisible = user.name.toLowerCase().includes(value)
     user.element.classList.toggle("hide", !isVisible)
   })
 })
@@ -20,10 +18,8 @@ fetch("https://xutilities.github.io/js-search-bar/database.json")
     users = data.map(user => {
       const card = userCardTemplate.content.cloneNode(true).children[0]
       const header = card.querySelector("[data-header]")
-      const body = card.querySelector("[data-body]")
       header.textContent = user.name
-      body.textContent = user.email
       userCardContainer.append(card)
-      return { name: user.name, email: user.email, element: card }
+      return { name: user.name, element: card }
     })
   })
